a30802254a3b71f83bca9748170c623e7be12763
klingondragon <klingondragon@hotmail.co.uk>
2024-11-02 10:09:42 +0000

    modified MD string

--- Pokémon Card DB to MD.user.js
+++ Pokémon Card DB to MD.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Pokémon Card DB to MD
-// @version      0.1.1
+// @version      0.1.2
 // @description  Adds a button to copy image, set, and number to clipboard as markdown table syntax
 // @namespace    https://github.com/KlingonDragon/user.js
 // @author       KlingonDragon
@@ -8,10 +8,15 @@
 // @run-at       document-end
 // ==/UserScript==
 document.querySelector('title')?.insertAdjacentText('afterbegin', `${window.location.pathname.split('/').slice(5, 7).join(' - ')} | `);
-
-const mdString = `|![${document.querySelector('div.card-description h1')?.textContent}](${document.querySelector('div.card-image > img')?.src})|![${document.querySelector('div.stats-footer h3')?.textContent}](${document.querySelector('div.expansion i')?.style.backgroundImage.replace(/url\("(.*)"\)/, '$1')})|${document.querySelector('div.stats-footer span')?.textContent?.replace(/(\S+)\/(\S+).*/, '$1/$2')}
+const mdString = `![${document.querySelector('div.card-description h1')?.textContent}](${document.querySelector('div.card-image > img')?.src})|![${document.querySelector('div.stats-footer h3')?.textContent}](${document.querySelector('div.expansion i')?.style.backgroundImage.replace(/url\("(.*)"\)/, '$1')})<br/>${document.querySelector('div.stats-footer span')?.textContent?.replace(/(\S+)\/(\S+).*/, '$1/$2').replace(/(\S+)\s.*/, '$1')}
 `, but = document.createElement('button');
 but.addEventListener('click', () => navigator.clipboard.writeText(mdString));
 but.innerText = 'Copy MD';
-but.style = 'position:fixed;top:0;left:0;font-size: 4rem;z-index:999999999';
+Object.assign(but.style, {
+    position: 'fixed',
+    top: '0',
+    left: '0',
+    fontSize: '4rem',
+    zIndex: '999999999'
+});
 document.querySelector('head')?.before(but);
